2560d535fe49d7084d8104019fd031acafd173c0
Isaac Wasserman <iwasserblaster@gmail.com>
2015-02-10 19:40:18 -0500

    ...

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,20 @@ var router = express.Router();
 var Message = require('../models/message');
 var request = require('request');
 var cheerio = require('cheerio');
-
+ 
 router.get('/:word', function(req, res) {
   var word = req.params.word;
-  
+ 
   request('http://dictionary.reference.com/browse/' + word, function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
-    $('.def-set').each(function(i, element){
-      var definition = $(this).text();
-      console.log(word + ": " + definition);
-    });
-  }
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      $('.def-set').each(function(i, element){
+        var definition = $(this).text().replace("(", "").replace(")", "");
+        console.log(word + ": " + definition);
+        res.render('def', {title: "Definition of:" + word, word: word, definition: definition});
+      });
+    }
+  });
 });
-  
-  res.render('def', {title: "Definition of:" + word, word: word, definition: definition});
-});
-
+ 
 module.exports = router;
-
-//request('http://dictionary.reference.com/browse/hello', function (error, response, html) {
-//  if (!error && response.statusCode == 200) {
-//    var $ = cheerio.load(html);
-//    $('.visible-xs').each(function(i, element){
-//      var a = $(this).text();
-//      console.log(a);
-//    });
-//  }
-//});
