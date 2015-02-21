d3479a24a9fb5033196168b835bf450048d5a0e6
Aharon Wasserman <aharon.wasserman@gmail.com>
2015-02-20 20:31:37 -0500

    fixing code so you get all definitions

--- routes/def.js
+++ routes/def.js
@@ -1,19 +1,25 @@
 var express = require('express');
-var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
  
+var router  = express.Router();
+
 router.get('/:word', function(req, res) {
   var word = req.params.word;
  
   request('http://dictionary.reference.com/browse/' + word, function (error, response, html) {
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
-      $('.def-set').each(function(i, element){
-        var definition = $(this).text().replace("(", "").replace(")", "");
-        console.log(word + ": " + definition);
-        res.render('def', {title: "Definition of: " + word, word: word, definition: definition});
-      });
+      var definitions  = [];
+      var $definitions = $('.def-set');
+
+      for(var x = 0; x < $definitions.length; x++) {
+        console.log($definitions[x]);
+        d = $($definitions[x]).text().replace("(", "").replace(")", "");
+        console.log(word + ": " + d);
+        definitions.push(d);
+      }
+      res.render('def', {title: "Definition of: " + word, word: word, definition: definitions});
     }
   });
   
