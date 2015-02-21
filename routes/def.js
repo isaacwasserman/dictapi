c65d5ff61205d75cf8c40e3f10f4c67e2f7c8601
Isaac Wasserman <iwasserblaster@gmail.com>
2015-02-20 21:23:16 -0500

    ...

--- routes/def.js
+++ routes/def.js
@@ -14,12 +14,19 @@ router.get('/:word', function(req, res) {
       var $definitions = $('.def-set');
 
       for(var x = 0; x < $definitions.length; x++) {
-        console.log($definitions[x]);
+        //console.log($definitions[x]);
         d = $($definitions[x]).text().replace("(", "").replace(")", "");
-        console.log(word + ": " + d);
         definitions.push(d);
       }
-      res.render('def', {title: "Definition of: " + word, word: word, definition: definitions});
+      
+      var def1 = definitions.slice(0,1);
+      var def1 = def1.substring(0,def1.indexOf(" "));
+      var def2 = definitions.slice(1,2);
+      var def2 = def2.substring(0,def2.indexOf(" "));
+      var def3 = definitions.slice(2,3);
+      var def3 = def3.substring(0,def3.indexOf(" "));
+      
+      res.render('def', {word: word, def1: def1, def2: def2, def3: def3});
     }
   });
   
