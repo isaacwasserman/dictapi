489c6a70b8808297783167c78bd5b118d73b508e
Aharon Wasserman <aharon.wasserman@gmail.com>
2015-02-20 20:21:05 -0500

    cleaning things up a bit

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+var express    = require('express');
+var path       = require('path');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/def');
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/messages');
+var routes     = require('./routes/def');
 
 var app = express();
 
@@ -16,7 +11,6 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -25,9 +19,9 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -35,32 +29,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', { message: err.message, error: err });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', { message: err.message, error: {} });
 });
 
-var server = app.listen(3000, function () {
-
+var server = app.listen(3000, function(){
   var host = server.address().address
   var port = server.address().port
-
   console.log("Dictapi is working it at localhost:" + port);
-
-})
+});
 
 module.exports = app;
